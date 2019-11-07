bbc6e530c79b389acb7dcd736c010f2775cb10e8
Alexander <alexanderattar@gmail.com>
2019-11-07 14:20:57 -0500

    Removing reference

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -25,11 +25,6 @@ module.exports = {
         title: "Web3",
         collapsable: false,
         children: ["/truffle/", "/infura/", "/metamask/", "/uport/", "/ipfs/"]
-      },
-      {
-        title: "Test",
-        collapsable: false,
-        children: ["/links/"]
       }
       // add new top level sections here...
     ],
